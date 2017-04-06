7fb2190466b8f511e7c1e47a4e490876c50abcc8
Nerys Jimenez Pichardo <jimenezpichardon785@strose.edu>
2017-04-05 22:54:51 -0400

    Update LexicographicList.java

--- LexicographicList.java
+++ LexicographicList.java
@@ -1,17 +1,16 @@
 
-package problem3;
 
 /**
- *
- * @author Nerys Jimenez
+ * Nerys Jimenez Pichardo
+ * The College of Saint Rose
+ * LexicographicList.java is a program that produces a list of all words ocurring in a given input text
+ * in lexicographic (dicionary) order and indicates the frequency of the ocurrence of each word.
+ * Input text may include puntuaction and white space characters.
  */
 
 import java.util.*; 
-public class Problem3 {
+public class LexicographicList{
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
     Scanner keyboard = new Scanner (System.in);
     ArrayList<String> list = new ArrayList<String>();
@@ -40,6 +39,7 @@ public class Problem3 {
     Arrays.fill(count, 1); //fill the list with 1s
     Collections.sort(list); //sort the list of words
     int sum = 0;
+        
     //taking each word in the list and doing a search to find duplicates
     //if duplicates then increase the count for that word
     for(int j = 0; j < list.size(); j++)
@@ -57,7 +57,7 @@ public class Problem3 {
         }
     }
       
-    //print out result
+    //print out each words in the sentence and its occurences
     for(int s = 0; s < list.size(); s ++)
     {
         System.out.println(list.get(s) +"("+count[s]+")");
